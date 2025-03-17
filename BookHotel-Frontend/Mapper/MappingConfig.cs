2900507fe32b482f689b9dd49d6cc95b44974735
kemish bajgai <kemishbajgai4@gmail.com>
2025-03-17 13:47:24 +0545

    1.AutoMapper configured between dtos in frontend section. 2.also added service url(url of backend project) in appsettings section. 3.default error viewpage removed.

---
+++ BookHotel-Frontend/Mapper/MappingConfig.cs
@@ -0,0 +1,24 @@
+﻿using AutoMapper;
+using BookHotel_Frontend.Models.DTOs;
+
+namespace BookHotel_Frontend.Mapper
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            //for hotel
+           
+          
+            CreateMap<HotelsDTO,HotelCreateDTO>().ReverseMap();
+            CreateMap<HotelsDTO,HotelUpdateDTO>().ReverseMap();
+
+            //for hotel numbers.
+         
+            CreateMap<HotelNoDTO, HotelNoCreateDTO>().ReverseMap();
+            CreateMap<HotelNoDTO, HotelNoUpdateDTO>().ReverseMap();
+
+            
+        }
+    }
+}
--- BookHotel-Frontend/Program.cs
+++ BookHotel-Frontend/Program.cs
@@ -1,8 +1,12 @@
+
+
+using BookHotel_Frontend.Mapper;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddAutoMapper(typeof(MappingConfig));
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
