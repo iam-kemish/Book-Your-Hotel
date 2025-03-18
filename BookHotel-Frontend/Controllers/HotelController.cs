9a33699f7c672eea997a0142cf9996bfb7e4cb22
kemish bajgai <kemishbajgai4@gmail.com>
2025-03-18 12:08:49 +0545

    -Created index viewpage and controller for hotels -Added "/" in url section in appsettings.

---
+++ BookHotel-Frontend/Controllers/HotelController.cs
@@ -0,0 +1,31 @@
+﻿using AutoMapper;
+using BookHotel_Frontend.Models;
+using BookHotel_Frontend.Models.DTOs;
+using BookHotel_Frontend.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace BookHotel_Frontend.Controllers
+{
+    public class HotelController : Controller
+    {
+        private readonly IHotelService _IHotel;
+        private readonly IMapper _IMapper;
+
+        public HotelController(IHotelService hotelService, IMapper mapper)
+        {
+            _IHotel = hotelService;
+            _IMapper = mapper;
+        }
+        public async Task<IActionResult>  Index()
+        {
+            List<HotelsDTO> ResultedList = new();
+            var response = await _IHotel.GetAllAsync<APIResponse>();
+            if (response != null && response.IsSuccess) {
+                ResultedList = JsonConvert.DeserializeObject<List<HotelsDTO>>(Convert.ToString(response.Result));
+            
+            }
+            return View(ResultedList);
+        }
+    }
+}
