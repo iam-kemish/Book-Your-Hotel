71d375da237baa0ae04009e4e3828d8d49c57070
kemish bajgai <kemishbajgai4@gmail.com>
2025-03-18 15:30:30 +0545

    Update and deletion implemented also added viewpages for it respectively.

--- BookHotel-Frontend/Controllers/HotelController.cs
+++ BookHotel-Frontend/Controllers/HotelController.cs
@@ -27,5 +27,75 @@ namespace BookHotel_Frontend.Controllers
             }
             return View(ResultedList);
         }
+
+        public async Task<IActionResult>  Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create(HotelCreateDTO hotelCreateDTO)
+        {
+            if (ModelState.IsValid)
+            {
+              
+                var response = await _IHotel.CreateAsync<APIResponse>(hotelCreateDTO);
+                if (response != null && response.IsSuccess)
+                {
+                  return RedirectToAction(nameof(Index));
+
+                }
+            }
+                return View(hotelCreateDTO);
+        }
+        public async Task<IActionResult> Update(int hotelId)
+        {
+            var response = await _IHotel.GetAsync<APIResponse>(hotelId);
+            if(response != null && response.IsSuccess)
+            {
+                HotelsDTO hotelsDTO = JsonConvert.DeserializeObject<HotelsDTO>(Convert.ToString(response.Result));
+                return View(_IMapper.Map<HotelUpdateDTO>(hotelsDTO));
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Update(HotelUpdateDTO hotelUpdateDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _IHotel.UpdateAsync<APIResponse>(hotelUpdateDTO);
+                if(response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(hotelUpdateDTO);
+        }
+        public async Task<IActionResult> Delete(int HotelId)
+        {
+            var response = await _IHotel.GetAsync<APIResponse>(HotelId);
+            if (response != null && response.IsSuccess)
+            {
+                HotelsDTO hotelsDTO = JsonConvert.DeserializeObject<HotelsDTO>(Convert.ToString(response.Result));
+                return View(hotelsDTO);
+            }
+            return NotFound();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Delete(HotelsDTO hotelsDTO)
+        {
+            if (ModelState.IsValid)
+            {
+                var response = await _IHotel.DeleteAsync<APIResponse>(hotelsDTO.Id);
+                if (response != null && response.IsSuccess)
+                {
+                    return RedirectToAction(nameof(Index));
+
+                }
+            }
+            return View(hotelsDTO);
+        }
     }
 }
