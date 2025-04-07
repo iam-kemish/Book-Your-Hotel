ee6596f570d14c55c3c395ce6c536ffac8dba19d
kemish bajgai <kemishbajgai4@gmail.com>
2025-04-07 11:48:34 +0545

    Added Session setup in controller and program.cs

--- BookHotel-Frontend/Controllers/AuthController.cs
+++ BookHotel-Frontend/Controllers/AuthController.cs
@@ -1,7 +1,9 @@
 ﻿using BookHotel_Frontend.Models;
 using BookHotel_Frontend.Models.DTOs;
 using BookHotel_Frontend.Services.IServices;
+using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace BookHotel_Frontend.Controllers
 {
@@ -23,7 +25,18 @@ namespace BookHotel_Frontend.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginRequestDTO loginRequestDTO)
         {
-            return View();
+            APIResponse aPIResponse = await _IAuth.LoginAsync<APIResponse>(loginRequestDTO);
+            if(aPIResponse != null && aPIResponse.IsSuccess)
+            {
+                var model = JsonConvert.DeserializeObject<LoginResponseDTO>(Convert.ToString(aPIResponse.Result));
+                HttpContext.Session.SetString("JWTToken", model.Token);
+                return RedirectToAction("Index", "Home");
+            }
+            else
+            {
+                ModelState.AddModelError("ErrorMessage", aPIResponse.Errors.FirstOrDefault());
+                return View(loginRequestDTO);
+            }
         }
         [HttpGet]
         public IActionResult Register() { 
@@ -42,6 +55,13 @@ namespace BookHotel_Frontend.Controllers
                 return RedirectToAction("Login");
             }
             return View();
+        }
+
+        public async Task<IActionResult> Logout()
+        {
+            await HttpContext.SignOutAsync();
+            HttpContext.Session.SetString("JWTToken", "");
+            return RedirectToAction("Index", "Home");
         }
 
     }
--- BookHotel-Frontend/Program.cs
+++ BookHotel-Frontend/Program.cs
@@ -14,6 +14,12 @@ builder.Services.AddScoped<IHotelService,HotelServiceClass>();
 builder.Services.AddScoped<IAuthService, AuthServiceClass>();
 builder.Services.AddHttpClient<IHotelNumberService, HotelNumberServiceClass>();
 builder.Services.AddScoped<IHotelNumberService, HotelNumberServiceClass>();
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromMinutes(100);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -28,7 +34,7 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
-
+app.UseSession();
 app.UseAuthorization();
 
 app.MapControllerRoute(
